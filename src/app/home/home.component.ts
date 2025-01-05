f689fb7c85a618fee664a70925b7e7877daf5865
MarkoMesel <mmarkom0001@gmail.com>
2025-01-05 18:25:12 +0100

    Added images; edited the home component so that the home page displays the added images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  images = [
+    { src: '/images/image1.jpg', alt: 'Image 1' },
+    { src: '/images/image2.jpg', alt: 'Image 2' },
+    { src: '/images/image3.jpg', alt: 'Image 3' },
+    { src: '/images/image4.jpg', alt: 'Image 4' },
+    { src: '/images/image5.jpg', alt: 'Image 5' },
+    { src: '/images/image6.jpg', alt: 'Image 6' },
+    { src: '/images/image7.jpg', alt: 'Image 7' },
+    { src: '/images/image8.jpg', alt: 'Image 8' },
+    { src: '/images/image9.jpg', alt: 'Image 9' },
+    { src: '/images/image10.jpg', alt: 'Image 10' },
+    { src: '/images/image11.jpg', alt: 'Image 11' },
+    { src: '/images/image12.jpg', alt: 'Image 12' }
+  ];
 }
