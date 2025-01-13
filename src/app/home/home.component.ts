0463259e0ea24d0810f58fdec07b0fd8f28485bb
MarkoMesel <mmarkom0001@gmail.com>
2025-01-13 15:46:23 +0100

    Modified the home component so that the home page displays the products retrieved from the database (their images via string path, names, categories and prices); modified the product component so that it displays the details for each product; renamed the scss class image-gallery to product-gallery; moved the image-item classes from home.component.scss to product.component.scss so that they can be used there; created new scss classes in product.component.scss for h2 and div, so that the spans inside of the div can be arranged into a row

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductComponent } from "../product/product.component";
 import { ProductService } from '../product.service';
+import { Product } from '../../types';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -12,24 +14,12 @@ import { ProductService } from '../product.service';
 export class HomeComponent {
   constructor(private productService: ProductService) {}
 
+  products: Product[] = [];
+
   ngOnInit() {
     this.productService.getProducts('http://localhost:3000/products', {}).subscribe((products) => {
-      console.log(products);
+      this.products = products.items;
+      console.log(products.items);
     })
   }
-
-  images = [
-    { src: '/images/image1.jpg', alt: 'Image 1' },
-    { src: '/images/image2.jpg', alt: 'Image 2' },
-    { src: '/images/image3.jpg', alt: 'Image 3' },
-    { src: '/images/image4.jpg', alt: 'Image 4' },
-    { src: '/images/image5.jpg', alt: 'Image 5' },
-    { src: '/images/image6.jpg', alt: 'Image 6' },
-    { src: '/images/image7.jpg', alt: 'Image 7' },
-    { src: '/images/image8.jpg', alt: 'Image 8' },
-    { src: '/images/image9.jpg', alt: 'Image 9' },
-    { src: '/images/image10.jpg', alt: 'Image 10' },
-    { src: '/images/image11.jpg', alt: 'Image 11' },
-    { src: '/images/image12.jpg', alt: 'Image 12' }
-  ];
 }
--- src/types.ts
+++ src/types.ts
@@ -34,7 +34,6 @@ export interface Product {
     rating: number,
     warranty: string,
     isFeatured: boolean
-
 }
 
 export interface Dimensions {
