85b8181bba70f2980b3f6f06377357433c4b032a
MarkoMesel <mmarkom0001@gmail.com>
2025-01-15 10:32:20 +0100

    Added @angular/material to project;  Added a dark theme; Added Edit and Delete buttons with icons for every loaded product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { Product } from '../../types';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
