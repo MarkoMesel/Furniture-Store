cad2aa46561d8f169834ce5c5114340b70ed51a8
MarkoMesel <mmarkom0001@gmail.com>
2025-01-16 12:01:26 +0100

    Merged the header css from header.component.css with the one in styles.css; Made the header always visible when scrolling; Fixed a bug where the header links moved slightly to the left when the home link is selected; Now only the content under the header is scrollable, instead of the entire page; Added an Add Product button to the left of the header items

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
