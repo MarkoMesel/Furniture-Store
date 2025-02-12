38200513d285b8e524965e314f13db965a28cc04
MarkoMesel <mmarkom0001@gmail.com>
2025-02-12 01:04:03 +0100

    Added Cart button to header Implemented cartItemCount property in header.component.ts using OnInit/OnDestroy

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { AddProductComponent } from "../add-product/add-product.component";
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../types';
 import { ProductService } from '../product.service';
 import { ToggleService } from '../toggle.service';
+import { Subscription } from 'rxjs';
+import { CartService } from '../cart.service';
 
 
 @Component({
@@ -15,11 +17,31 @@ import { ToggleService } from '../toggle.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  constructor(private productService: ProductService, private toggleService: ToggleService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  cartItemCount = 0;
+  private subscription!: Subscription;
+  
+  constructor(private productService: ProductService, private toggleService: ToggleService, private cartService: CartService) {}
 
   showForm(): void {
     this.toggleService.toggleAddOrEditProductVisible(true);
     this.toggleService.toggleIsEditProduct(false);
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.cartService.cartItems$.subscribe(cartItems => {
+    this.cartItemCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+    });
+  }
+
+  openCart(): void {
+    // TODO: Implement logic
+    console.log('Opening Cart...');
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
