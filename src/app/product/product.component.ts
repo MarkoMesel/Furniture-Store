21354589de8fcfd5559cf74e69b7b5337159f64c
MarkoMesel <mmarkom0001@gmail.com>
2025-02-12 10:33:12 +0100

    Added Add-to-Cart button and quantity input to all products Implemented addToCart in product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { Product } from '../../types';
 import { MatIconModule } from '@angular/material/icon';
+import { CartService } from '../cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
@@ -13,7 +15,13 @@ export class ProductComponent {
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+  selectedQuantity: number = 1;
 
+  constructor(private cartService: CartService) {}
+
+  addToCart(): void {
+    this.cartService.addToCart(this.product, this.selectedQuantity);
+  }
   
   editProduct(product: Product) {
     this.edit.emit(this.product);
