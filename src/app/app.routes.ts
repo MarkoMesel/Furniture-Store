434197920fdf2d0bbf9f600c054448f287b08501
MarkoMesel <mmarkom0001@gmail.com>
2025-02-12 13:53:47 +0100

    Added a new CartComponent Added a route for CartComponent in app.routes.ts Updated the Cart button in the header to use routerLink and routerLinkActive; removed the `(click)` event binding.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'cart', component: CartComponent }
 ];
