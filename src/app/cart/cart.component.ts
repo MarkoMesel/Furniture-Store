287a0a07beaa491051cc069e8079393875678734
MarkoMesel <mmarkom0001@gmail.com>
2025-02-13 18:59:39 +0100

    Added a table to the cart page displaying all cart items with their details and the total price Implemented a function in cart.component.ts to calculate the total price of items in the cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CartItem } from '../../types';
+import { Subscription } from 'rxjs';
+import { CartService } from '../cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent {
+export class CartComponent implements OnInit, OnDestroy {
+  cartItems: CartItem[] = [];
+  private subscription!: Subscription;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    // Subscribe to the cartItems observable to get updates
+    this.subscription = this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+    });
+  }
+
+  // Calculate the overall total price of the cart
+  getTotal(): number {
+    return this.cartItems.reduce((total, item) => 
+      total + (item.product.price * item.quantity), 0
+    );
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe to prevent memory leaks
+    this.subscription.unsubscribe();
+  }
 }
